bf2825a5416d9d107162e882a2491c9bce9c20e9
KierMc <kiernanmcvay@gmail.com>
2022-10-31 12:27:31 -0700

    first commit - more forms

--- src/components/HookForm.jsx
+++ src/components/HookForm.jsx
@@ -8,9 +8,98 @@ const HookForm = () => {
     const [password,setPassword] = useState("")
     const [confirm,setConfirm] = useState("")
 
+    const [name,setName] = useState("")
+    const [nameError, setNameError] = useState("")
+    const [emailError, setEmailError] = useState("")
+    const [passwordError,setPasswordError] = useState("")
+    const [confirmError,setConfirmError] = useState("")
+
+
+    const handleName = (e) => {
+        setName(e.target.value);
+        if (e.target.value.length<2 && e.target.value.length > 0){
+            setNameError("Name must be at least 2 characters");
+        }
+        else{
+            setNameError("");
+        }
+    }
+    const handleEmail =(e) => {
+        setEmail(e.target.value);
+        if (e.target.value.length < 5 && e.target.value.length > 0){
+            setEmailError("Email must be at least 5 characters")
+        }
+        else{
+            setEmailError("")
+        }
+    }
+    const handlePassword =(e) => {
+        setPassword(e.target.value);
+        if (e.target.value.length < 8 && e.target.value.length > 0){
+            setPasswordError("Password must be at least 8 characters")
+        }
+        else{
+            setPasswordError("")
+        }
+    }
+    // const matchPassword =(e) => {
+    //     setConfirm(e.target.value);
+    //     if (confirm !== "" && password !== confirm){
+    //         setConfirmError("Password must match")
+    //     }
+    //     else{
+    //         setConfirmError("")
+    //     }
+    // }
+
     return (
         <div>
             <form>
+                <div>
+                    <label>First Name: </label>
+                    <input onChange={handleName} type="text"></input>
+                    {
+                        nameError ?
+                        <p style={{color:'red'}}>{ nameError }</p> : ''
+                    }
+                </div>
+                <div>
+                    <label>Last Name: </label>
+                    <input onChange={handleName} type="text"></input>
+                    {
+                        nameError ?
+                        <p style={{color:'red'}}>{ nameError }</p> : ''
+                    }
+                </div>
+                <div>
+                    <label>Email: </label>
+                    <input onChange={handleEmail} type="email"></input>
+                    {
+                        emailError ?
+                        <p style={{color:'red'}}>{ emailError }</p> : ''
+                    }
+                </div>
+                <div>
+                    <label>Password: </label>
+                    <input onChange={handlePassword} type="text"></input>
+                    {
+                        passwordError ?
+                        <p style={{color:'red'}}>{ passwordError }</p> : ''
+                    }
+                </div>
+                <div>
+                    <label>Confirm Password: </label>
+                    <input onChange={(e) => setConfirm(e.target.value)} type="text"></input>
+                    {
+                        confirm !== "" && password !== confirm ? <p style={{color:'red'}}>Passwords must be the same</p> : ''
+                    }
+                    {/* {
+                        confirmError ?
+                        <p style={{color:'red'}}>{confirmError}</p> : ''
+                    } */}
+                </div>
+            </form>
+            {/* <form>
                 <div>
                     <label>First Name: </label>
                     <input onChange={(e) => setFirst(e.target.value)} type="text"></input>
@@ -31,14 +120,14 @@ const HookForm = () => {
                     <label>Confirm Password: </label>
                     <input onChange={(e) => setConfirm(e.target.value)} type="password"></input>
                 </div>
-            </form>
+            </form> */}
 
-            <h3>Your Form Data: </h3>
+            {/* <h3>Your Form Data: </h3>
             <p>First Name: {first}</p>
             <p>Last Name: {last}</p>
             <p>Email: {email}</p>
             <p>Password: {password}</p>
-            <p>Confirm Password: {confirm}</p>
+            <p>Confirm Password: {confirm}</p> */}
         </div>
     )
 }
